c516d65183c61d0b641c77db9c5b3999f8277850
Mahati <mahatisayee@gmail.com>
2026-06-28 12:35:55 +0530

    Fix User model import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {Routes,Route} from 'react-router';
+import {Routes,Route} from 'react-router-dom';
 import './App.css';
 import { Navigate } from "react-router-dom";
 import Register from './pages/register';
